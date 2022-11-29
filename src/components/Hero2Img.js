6c8fbbeb6b6a2825cbe902932872de7233b60e3e
AmandaFrisk <amanda.frisk33@gmail.com>
2022-11-28 17:10:32 -0700

    re styling about pg

--- src/components/Hero2Img.js
+++ src/components/Hero2Img.js
@@ -1,20 +1,49 @@
 
 import React from 'react'
 import "./Hero2Styles.css"
-// import hero2Background from "../assets/hero2Background.jpeg"
+import hero2Background from "../assets/hero2Background.jpeg"
 import lifesaver from "../assets/lifesaver.jpg"
-import fashion from "../assets/fashion.jpeg"
-import awareness from "../assets/awareness.jpeg"
+// import fashion from "../assets/fashion.jpeg"
+// import awareness from "../assets/awareness.jpeg"
 // import {Link} from "react-router-dom"
-import {Carousel} from 'react-bootstrap';
+// import {Carousel} from 'react-bootstrap';
 
 function Hero2Img() {
 
-    return (
-      <>
-      {/* <h3>ABOUT</h3> */}
-      <div className="hero2img">
-      <Carousel fade>
+  return (
+    
+        <div className ="hero-2">
+        <div className="mask-2">
+          <img className="hero2img"
+          src={hero2Background} alt="background" /> 
+        </div>
+        <div className="about-content-1">
+          <div className="title-container-1">
+            <h1 className="backstory">BACKSTORY</h1>
+         </div>
+        <div className="content-container">
+
+            <div className="about-img-container">    
+                    <img className="about-img"src ={lifesaver} alt='about'/>
+            </div>
+            <div className="about-info-container">
+            <p className="hero2-p">Kristian was working out at his local gym in Greeley & by fate met Dwight Randolph. Upon meeting, Kristian discovered that Dwight had late stage kidney failure and in urgent need of a lifesaving kidney transplant. 
+          In that very moment Kristian offered to get tested. Kristian was in fact a match and he donated his kidney to save Dwight's life. Kristian 
+          was so deeply moved by the experience that he has now dedicated his life to finding organ donors through his charity: #GoSaveALife</p>
+
+            </div>
+          </div>
+
+          </div>
+          </div>
+       )}
+
+          export default Hero2Img
+
+       /* <> */
+      /* <h3>ABOUT</h3> */
+      /* <div className="hero2img">
+      <Carousel fade >
         <Carousel.Item>
           <img
             className="d-block w-100"
@@ -57,42 +86,8 @@ function Hero2Img() {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-      </div>
-      </>
-    );
-  }
+      </div> */
+      
+   
   
-
-
-//   return (
-    
-//     <div className ="hero">
-//     <div className="mask">
-//       <img className="hero2img"
-//       src={hero2Background} alt="background" /> 
-//     </div>
-//     <div className="content">
-//       <div className="title-container">
-//         <h1>ABOUT</h1>
-//         </div>
-//         <div className="about-img-container">    
-//                 <img className="about-img"src ={lifesaver} alt='about'/>
-//         </div>
-//         <div className="about-info-container">
-//         <p>Kristian was working out at his local gym in Greeley & by fate met Dwight Randolph. Upon meeting, Kristian discovered that Dwight had late stage kidney failure and in urgent need of a lifesaving kidney transplant.</p> 
-//          <p> In that very moment Kristian offered to get tested. Kristian was in fact a match and he donated his kidney to save Dwight's life. Kristian 
-//          was so deeply moved by the experience that he has now dedicated his life to finding organ donors through his charity: #GoSaveALife</p>
-//          </div>
-//         {/* <div className="hero-btn-container">
-//         <a href="https://docs.google.com/forms/d/e/1FAIpQLSdXWz_LOPWMUUyTefH3-3R2u4myBsZyhB9EuPY-UW_kdQVWVw/viewform"
-//         className="donor-btn">Become A Living Donor </a>
-//         </div> */}
-        
-        
-//     </div>
-//     </div>
-    
-//   )
-// }
-
-export default Hero2Img
+  
