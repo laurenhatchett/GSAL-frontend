8cde6541b647a30327277ed26ddbfa5f291afe44
AmandaFrisk <amanda.frisk33@gmail.com>
2022-11-29 11:58:15 -0700

    last css to about

--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -1,27 +1,19 @@
 import "./WhatWeDoStyles.css"
 import React from 'react'
-import fashion1 from '../assets/fashion1.jpeg'
-import fashion2 from '../assets/fashion2.jpeg'
+
 import fashion3 from '../assets/fashion3.jpeg'
-import fashion4 from '../assets/fashion4.jpeg'
 import fashion5 from '../assets/fashion5.jpeg'
-import fashion6 from '../assets/fashion6.jpeg'
+import fashion from '../assets/fashion.jpeg'
 import fashion7 from '../assets/fashion7.jpeg'
-import fashion8 from '../assets/fashion8.jpeg'
-import fashion9 from '../assets/fashion9.jpeg'
-import fashion10 from '../assets/fashion10.jpeg'
-import fashion11 from '../assets/fashion11.jpeg'
 import fashion12 from '../assets/fashion12.jpeg'
 import fashion13 from '../assets/fashion13.jpeg'
 import awareness1 from '../assets/awareness1.png'
 import awareness2 from '../assets/awareness2.jpeg'
 import awareness3 from '../assets/awareness3.jpeg'
-import awareness4 from '../assets/awareness4.jpeg'
-import awareness5 from '../assets/awareness5.png'
 import awareness6 from '../assets/awareness6.jpeg'
 import awareness7 from '../assets/awareness7.png'
 import awareness8 from '../assets/awareness8.jpeg'
-import awareness9 from '../assets/awareness9.png'
+
 
 const WhatWeDo = () => {
   return (
@@ -31,7 +23,7 @@ const WhatWeDo = () => {
     </div>
 
     <div className="why-statement-container">
-    <h2 className="why-statement">Kristian and Dwight have shared their story on several news stations and have given speeches at hospital conferences. They have also interviewed live donors and individuals in need. Follow us on social media platforms for more content. </h2>
+    <h2 className="why-statement">Kristian and Dwight have shared their story on several news stations and have given speeches at hospital conferences. They have also interviewed live donors and individuals in need.</h2>
     </div>
     <div className="wrapper">
       <div className="item">
@@ -46,14 +38,8 @@ const WhatWeDo = () => {
         <img className="item-img"
           src={awareness3} alt="fashion" /> 
     </div>
-    <div className="item">
-        <img className="item-img"
-          src={awareness4} alt="fashion" /> 
-    </div>
-    <div className="item">
-        <img className="item-img"
-          src={awareness5} alt="fashion" /> 
-    </div>
+    
+    
     <div className="item">
         <img className="item-img"
           src={awareness6} alt="fashion" /> 
@@ -66,63 +52,36 @@ const WhatWeDo = () => {
         <img className="item-img"
           src={awareness8} alt="fashion" /> 
     </div>
-    <div className="item">
-        <img className="item-img"
-          src={awareness9} alt="fashion" /> 
-    </div>
+    
     
   
     </div>
 
 
     <div className="why-statement-container">
-    <h2 className="why-statement">Go Save A Life produces fashion shows with the mission to leverage the platforms of models, designers and photographers. This has proven to be a successful outreach as it has connected several people with live donors whom saved their lives! </h2>
+    <h2 className="why-statement">Go Save A Life produces fashion shows with the mission to leverage the platforms of models, designers and photographers. Follow us on social media platforms for event information. </h2>
     </div>
   <div className="wrapper">
-    <div className="item">
-      <img className="item-img"
-          src={fashion1} alt="fashion" /> 
-    </div>
-    <div className="item">
-      <img className="item-img"
-          src={fashion2} alt="fashion" /> 
-    </div>
+    
+    
     <div className="item">
       <img className="item-img"
           src={fashion3} alt="fashion" /> 
     </div>
-    <div className="item">
-      <img className="item-img"
-          src={fashion4} alt="fashion" /> 
-    </div>
+    
     <div className="item">
       <img className="item-img"
           src={fashion5} alt="fashion" /> 
     </div>
     <div className="item">
       <img className="item-img"
-          src={fashion6} alt="fashion" /> 
+          src={fashion} alt="fashion" /> 
     </div>
     <div className="item">
       <img className="item-img"
           src={fashion7} alt="fashion" /> 
     </div>
-    <div className="item">
-      <img className="item-img"
-          src={fashion8} alt="fashion" /> 
-    </div>
-    <div className="item">
-      <img className="item-img"
-          src={fashion9} alt="fashion" /> 
-    </div>
-    <div className="item">
-      <img className="item-img"
-          src={fashion10} alt="fashion" /> 
-    </div>
-    <div className="item">
-      <img className="item-img"
-          src={fashion11} alt="fashion" /> 
-    </div>
+    
     <div className="item">
       <img className="item-img"
           src={fashion12} alt="fashion" /> 
